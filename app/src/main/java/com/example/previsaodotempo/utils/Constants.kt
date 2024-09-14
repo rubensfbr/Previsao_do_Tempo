73f04fa04922685ec4740502cfc2d45e18bd34bd
rubensfbr <rubens_fbr@hotmail.com>
2024-09-14 16:36:35 -0300

    initial commit

--- app/src/main/java/com/example/previsaodotempo/utils/Constants.kt
+++ app/src/main/java/com/example/previsaodotempo/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.example.previsaodotempo.utils
 
 object Constants {
 
-    const val API_KEY = "127c249b6aed8219f78ef697a804c287"
+    const val API_KEY = "Put your api key here"
     const val BASE_URL = "https://api.openweathermap.org"
     const val ICON_URL = "https://openweathermap.org/img/wn/"
 
